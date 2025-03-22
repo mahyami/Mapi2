410166d8cca0f48a38207cdd9de59f21d7aa288a
Mahya Mirhabibi <mahya.mirhabibi@qonto.com>
2025-03-22 20:09:43 +0100

    add resources

---
+++ app/src/main/java/com/example/mapi/ui/theme/Colors.kt
@@ -0,0 +1,12 @@
+package com.example.mapi.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object Colors {
+    val Primary = Color(0xFF00284F)
+    val Secondary = Color(0xFF009688)
+    val Tertiary = Color(0xFFDCD1E2)
+    val White = Color(0xFFFFFFFF)
+    val TransparentWhite = Color(0xB5FFFFFF)
+    val BackgroundTint = Color(0xD02F2D2D)
+}
---
+++ app/src/main/java/com/example/mapi/ui/theme/MapiTheme.kt
@@ -0,0 +1,44 @@
+package com.example.mapi.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Typography
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MapiTheme(
+    content: @Composable () -> Unit
+) {
+    val colors = lightColorScheme(
+        primary = Colors.Primary,
+        secondary = Colors.Secondary,
+        tertiary = Colors.Tertiary,
+        background = Colors.White
+    )
+    val typography = Typography(
+        bodyMedium = TextStyle(
+            fontFamily = FontFamily.Default,
+            fontWeight = FontWeight.Normal,
+            fontSize = 16.sp
+        )
+    )
+    val shapes = Shapes(
+        small = RoundedCornerShape(4.dp),
+        medium = RoundedCornerShape(4.dp),
+        large = RoundedCornerShape(0.dp)
+    )
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = typography,
+        shapes = shapes,
+        content = content
+    )
+}
